17a3c28e22d95a43b0d896134abb8961fa142074
elimalone19 <elimalone19@gmail.com>
2021-02-03 23:56:18 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ApplicationResponse AppResponse)
+        {
+            if (!ModelState.IsValid)
+            { //checking model state
+                return View(AppResponse);
+            }
+            Temp.AddApplication(AppResponse);
+            return View("Confirmation");
+        }
+
+        public IActionResult Movies()
+        {
+            return View(Temp.Applications);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ApplicationResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models
+{
+    public class ApplicationResponse
+    {
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+        public string Lent { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
